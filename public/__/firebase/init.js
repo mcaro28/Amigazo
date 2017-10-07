6b6fb278c7e0fe99a6f0f6ef4ca4447b00f0a392
mcaro28 <lafemiguel@gmail.com>
2017-10-07 08:49:55 -0500

    borrando carpeta

--- public/__/firebase/init.js
+++
@@ -1,9 +0,0 @@
-if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
-firebase.initializeApp({
-  "apiKey": "AIzaSyAoMAslbn009jgqIz-32N0i0NEdPOgghIg",
-  "databaseURL": "https://amigazo-74726.firebaseio.com",
-  "storageBucket": "amigazo-74726.appspot.com",
-  "authDomain": "amigazo-74726.firebaseapp.com",
-  "messagingSenderId": "973119316235",
-  "projectId": "amigazo-74726"
-});
